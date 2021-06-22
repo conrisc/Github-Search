6024a3cfc9525e3af730a65007b2d4f78a3e9352
Konrad Cielecki <conrisc@gmail.com>
2021-06-22 14:32:10 +0200

    Add svg with msg about empty list to SimpleList component

--- src/components/SimpleList/index.tsx
+++ src/components/SimpleList/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import styles from './SimpleList.module.sass';
 
+import styles from './SimpleList.module.sass';
 import { ListItem } from './ListItem';
+import emptySvg from 'assets/empty.svg';
 
 export interface Row {
 	id: string | number;
@@ -24,9 +25,17 @@ export function SimpleList(props: SimpleListProps): JSX.Element {
 				<h3 className={styles.listHeader}>{header}</h3>
 			}
 			<div className={styles.listContent}>
-				{data.map((r: Row) =>
-					<ListItem key={r.id} label={r.label} item={r.item} onClick={onRowClick} />
-				)}
+				{data.length > 0
+					?  data.map((r: Row) =>
+						<ListItem key={r.id} label={r.label} item={r.item} onClick={onRowClick} />
+					)
+					: (
+						<div className={styles.emptyListContainer}>
+							<h3>The list is empty</h3>
+							<img className={styles.emptyListSvg} src={emptySvg} alt="List is empty" />
+						</div>
+					)
+				}
 			</div>
 		</div>
 	);
