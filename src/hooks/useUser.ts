c89f722952d01d1577d54218c77f924a3162f0e2
Konrad Cielecki <conrisc@gmail.com>
2021-06-22 11:58:55 +0200

    Implements useUser hook

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+import { UserDetails } from 'types/User';
+
+interface UserResult {
+	user: UserDetails | undefined;
+	loading: boolean;
+}
+
+const endpoint = 'https://api.github.com/users/';
+
+export function useUser(username: string = ''): UserResult {
+	const [user, setUser] = useState<UserDetails | undefined>();
+	const [loading, setLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (!username) return;
+
+		const url = new URL(endpoint + username);
+
+		setLoading(true);
+		fetch(url.toString())
+			.then(async (r: Response) => {
+				const usersData: UserDetails = await r.json();
+				setUser(usersData);
+			})
+			.catch(() => {
+				console.error('Couldn\'t fetch user');
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+
+	}, [username]);
+
+	return {
+		user,
+		loading,
+	};
+}
--- src/types/User.ts
+++ src/types/User.ts
@@ -9,3 +9,8 @@ export interface User {
 	avatar_url: string;
 	repos_url: string;
 }
+
+export interface UserDetails extends User {
+	name: string;
+	bio: string;
+}
